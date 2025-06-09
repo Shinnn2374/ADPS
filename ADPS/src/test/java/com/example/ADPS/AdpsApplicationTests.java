55fcc4fc9fa8d597201e04caf9770ed940f84b16
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-09 17:14:10 +0300

    ...

---
+++ ADPS/src/test/java/com/example/ADPS/AdpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ADPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
